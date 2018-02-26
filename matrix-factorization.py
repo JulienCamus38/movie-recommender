675513b01047e826f44f06a67f7acc433aede0f6
JulienCamus38 <camusjulien40@gmail.com>
2018-02-26 12:35:23 +0100

    First implementation not optimized

--- matrix-factorization.py
+++ matrix-factorization.py
@@ -5,5 +5,227 @@ Created on Mon Jan 29 16:12:54 2018
 @author: camusj
 """
 
+# Imports
+try:
+    import numpy as np
+    np.random.seed(0)
+except ImportError:
+    raise ImportError('numpy module needs to be imported.')
+    
+try:
+    from sklearn.metrics import mean_squared_error
+except ImportError:
+    raise ImportError('sklearn.metrics module needs to be imported.')
+                      
+try:
+    import parser
+except ImportError:
+    raise ImportError('parser module needs to be imported.')
+                      
+try:
+    import matplotlib.pyplot as plt
+except ImportError:
+    raise ImportError('matplotlib.pyplot module needs to be imported.')
+                      
+try:
+    import seaborn as sns
+    sns.set()
+except ImportError:
+    raise ImportError('seaborn module needs to be imported.')
+
+def get_mse(pred, actual):
+    """
+    """
+    
+    # Ignore nonzero terms.
+    pred = pred[actual.nonzero()].flatten()
+    actual = actual[actual.nonzero()].flatten()
+    return mean_squared_error(pred, actual)
+
+def plot_learning_curve(iter_array, model):
+    """
+    """
+    
+    plt.plot(iter_array, model.train_mse, label='Training', linewidth=5)
+    plt.plot(iter_array, model.test_mse, label='Testing', linewidth=5)
+
+    plt.xticks(fontsize=16);
+    plt.yticks(fontsize=16);
+    
+    plt.xlabel('iterations', fontsize=30);
+    plt.ylabel('MSE', fontsize=30);
+    
+    plt.legend(loc='best', fontsize=20);
+
 class mf():
-    def __init__(self, nb_factors=40, learning='sgd', movie_fact_reg=0.0)
+    def __init__(self, 
+                 ratings,
+                 n_factors=40,
+                 item_fact_reg=0.0, 
+                 user_fact_reg=0.0,
+                 item_bias_reg=0.0,
+                 user_bias_reg=0.0,
+                 verbose=False):
+        """
+        Train a matrix factorization model to predict empty 
+        entries in a matrix. The terminology assumes a 
+        ratings matrix which is ~ user x item
+        
+        Params
+        ======
+        ratings : (ndarray)
+            User x Item matrix with corresponding ratings
+        
+        n_factors : (int)
+            Number of latent factors to use in matrix 
+            factorization model
+        
+        item_fact_reg : (float)
+            Regularization term for item latent factors
+        
+        user_fact_reg : (float)
+            Regularization term for user latent factors
+            
+        item_bias_reg : (float)
+            Regularization term for item biases
+        
+        user_bias_reg : (float)
+            Regularization term for user biases
+        
+        verbose : (bool)
+            Whether or not to printout training progress
+        """
+        
+        self.ratings = ratings
+        self.n_users, self.n_items = ratings.shape
+        self.n_factors = n_factors
+        self.item_fact_reg = item_fact_reg
+        self.user_fact_reg = user_fact_reg
+        self.item_bias_reg = item_bias_reg
+        self.user_bias_reg = user_bias_reg
+        self.sample_row, self.sample_col = self.ratings.nonzero()
+        self.n_samples = len(self.sample_row)
+        self._v = verbose
+
+    def train(self, n_iter=10, learning_rate=0.1):
+        """ Train model for n_iter iterations from scratch."""
+        # initialize latent vectors        
+        self.user_vecs = np.random.normal(scale=1./self.n_factors,\
+                                          size=(self.n_users, self.n_factors))
+        self.item_vecs = np.random.normal(scale=1./self.n_factors,
+                                          size=(self.n_items, self.n_factors))
+        
+        self.learning_rate = learning_rate
+        self.user_bias = np.zeros(self.n_users)
+        self.item_bias = np.zeros(self.n_items)
+        self.global_bias = np.mean(self.ratings[np.where(self.ratings != 0)])
+        self.partial_train(n_iter)
+    
+    
+    def partial_train(self, n_iter):
+        """ 
+        Train model for n_iter iterations. Can be 
+        called multiple times for further training.
+        """
+        ctr = 1
+        while ctr <= n_iter:
+            if ctr % 10 == 0 and self._v:
+                print('\tcurrent iteration: {}'.format(ctr))
+            self.training_indices = np.arange(self.n_samples)
+            np.random.shuffle(self.training_indices)
+            self.sgd()
+            ctr += 1
+
+    def sgd(self):
+        for idx in self.training_indices:
+            u = self.sample_row[idx]
+            i = self.sample_col[idx]
+            prediction = self.predict(u, i)
+            e = (self.ratings[u,i] - prediction) # error
+            
+            # Update biases
+            self.user_bias[u] += self.learning_rate * \
+                                (e - self.user_bias_reg * self.user_bias[u])
+            self.item_bias[i] += self.learning_rate * \
+                                (e - self.item_bias_reg * self.item_bias[i])
+            
+            #Update latent factors
+            self.user_vecs[u, :] += self.learning_rate * \
+                                    (e * self.item_vecs[i, :] - \
+                                     self.user_fact_reg * self.user_vecs[u,:])
+            self.item_vecs[i, :] += self.learning_rate * \
+                                    (e * self.user_vecs[u, :] - \
+                                     self.item_fact_reg * self.item_vecs[i,:])
+                                     
+    def predict(self, u, i):
+        """ Single user and item prediction."""
+        prediction = self.global_bias + self.user_bias[u] + self.item_bias[i]
+        prediction += self.user_vecs[u, :].dot(self.item_vecs[i, :].T)
+        return prediction
+    
+    def predict_all(self):
+        """ Predict ratings for every user and item."""
+        predictions = np.zeros((self.user_vecs.shape[0], 
+                                self.item_vecs.shape[0]))
+        for u in range(self.user_vecs.shape[0]):
+            for i in range(self.item_vecs.shape[0]):
+                predictions[u, i] = self.predict(u, i)
+                
+        return predictions
+    
+    def calculate_learning_curve(self, iter_array, test, learning_rate=0.1):
+        """
+        Keep track of MSE as a function of training iterations.
+        
+        Params
+        ======
+        iter_array : (list)
+            List of numbers of iterations to train for each step of 
+            the learning curve. e.g. [1, 5, 10, 20]
+        test : (2D ndarray)
+            Testing dataset (assumed to be user x item).
+        
+        The function creates two new class attributes:
+        
+        train_mse : (list)
+            Training data MSE values for each value of iter_array
+        test_mse : (list)
+            Test data MSE values for each value of iter_array
+        """
+        iter_array.sort()
+        self.train_mse = []
+        self.test_mse = []
+        iter_diff = 0
+        for (i, n_iter) in enumerate(iter_array):
+            if self._v:
+                print('Iteration: {}'.format(n_iter))
+            if i == 0:
+                self.train(n_iter - iter_diff, learning_rate)
+            else:
+                self.partial_train(n_iter - iter_diff)
+
+            predictions = self.predict_all()
+
+            self.train_mse += [get_mse(predictions, self.ratings)]
+            self.test_mse += [get_mse(predictions, test)]
+            if self._v:
+                print('Train mse: ' + str(self.train_mse[-1]))
+                print('Test mse: ' + str(self.test_mse[-1]))
+            iter_diff = n_iter
+            
+    # Main function (in order to test our implementation)
+if __name__ == '__main__':
+    
+    # Parse the CSV file into a list of dictionaries
+    ratings = parser.parse_csv()
+    
+    # Split into a train and a test arrays
+    train, test = parser.train_test_split(ratings)
+    
+    # Calculation
+    mf = mf(train, 40, verbose=True)
+    iter_array = [1, 2, 5, 10, 25, 50, 100, 200]
+    mf.calculate_learning_curve(iter_array, test, learning_rate=0.001)
+    
+    # Plot
+    plot_learning_curve(iter_array, mf)
--- parser.py
+++ parser.py
@@ -4,6 +4,7 @@ Created on Mon Jan 29 16:12:50 2018
 
 @author: camusj
 """
+
 try:
     import sys
     import numpy as np
@@ -11,6 +12,65 @@ try:
     np.random.seed(0)
 except ImportError:
     raise ImportError('sys, numpy and pandas modules need to be imported.')
+    
+def create_dict_users(ratings):
+    """
+    """
+    
+    # Initialization
+    dict_users = dict()
+    fname = './ml-latest-small/ratings.csv'
+    
+    # Try/catch if file exists or not
+    try:
+        f = open(fname, 'r')
+    except IOError:
+        print("Could not read file: " + fname)
+        sys.exit()
+    
+    with f:
+        names = ['userId', 'movieId', 'rating', 'timestamp']
+        df = pd.read_csv(f, names=names, skiprows=1)
+        
+        for row in df.itertuples():
+            key = int(row[0])
+            if key in dict_users.keys():
+                dict_users[key].append(row[1:3])
+            else:
+                dict_users[key] = [row[1:3]]
+    
+    return dict_users
+
+def create_dict_indexes(ratings):
+    """
+    """
+    
+    # Initialization
+    dict_indexes = dict()
+    fname = './ml-latest-small/ratings.csv'
+    key = 0
+    
+    # Try/catch if file exists or not
+    try:
+        f = open(fname, 'r')
+    except IOError:
+        print("Could not read file: " + fname)
+        sys.exit()
+    
+    with f:
+        names = ['userId', 'movieId', 'rating', 'timestamp']
+        df = pd.read_csv(f, names=names, skiprows=1)
+        
+        for row in df.itertuples():
+            value = int(row[1])
+            if value not in dict_indexes.values():
+                dict_indexes[key] = value
+                ++key
+                
+    return dict_indexes
+    
+def get_length_dict_users(dict_users, i):
+    return len(dict_users[i])
 
 # Parse the csv file and create the array of ratings: [[userId, movieId, rating]]
 def parse_csv(fname='./ml-latest-small/ratings.csv'):
@@ -38,6 +98,8 @@ def parse_csv(fname='./ml-latest-small/ratings.csv'):
         
         # Initialize the ratings array
         ratings = np.zeros((nb_users, nb_movies))
+        
+        # Filling
         for row in df.itertuples():
             ratings[int(row[1])-1, int(row[2])-1] = float(row[3])
         
@@ -82,12 +144,3 @@ def train_test_split(ratings):
     assert(np.all((train * test) == 0))
     
     return train, test
-    
-# Main function (in order to test our implementation)
-if __name__ == '__main__':
-    
-    # Parse the CSV file into a list of dictionaries
-    ratings = parse_csv()
-    
-    # Split into a train and a test arrays
-    train, test = train_test_split(ratings)
